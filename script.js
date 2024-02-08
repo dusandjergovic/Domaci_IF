bcd85ef8a925f840ebe6c7e8767b62b8b04cb4c4
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-02-08 11:53:11 +0100

    Odradjen domaci za IF-ove

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// // BOJA DUGMETA
+let dugme = prompt('Unesi boju dugmeta').toLowerCase();
+let btn = document.querySelector('#dugme');
+
+if (dugme == 'plava') {
+    btn.style.backgroundColor = 'blue';
+} else if (dugme == 'crvena') {
+    btn.style.backgroundColor = 'red';
+} else {
+    btn.style.backgroundColor = 'green';
+};
+
+// BOJA TEKSTA
+let bojaTeksta = prompt('Unesi boju teksta').toLowerCase();
+
+if (bojaTeksta == 'plava') {
+    btn.style.color = 'blue';
+} else if(bojaTeksta == 'crvena') {
+    btn.style.color = 'red';
+};
+
+// VISINA DUGMETA
+let visina = prompt('Unesite visinu dugmeta');
+if(visina >= 50 ) {
+    btn.style.height = visina + 'px';
+} else if (visina < 50){
+    alert('Uneli ste pogresne mere')
+}
+
+// SIRINA DUGMETA
+let sirina = prompt('Unesite sirinu dugmeta');
+if(sirina >= 50 ) {
+    btn.style.width = sirina + 'px';
+} else if (sirina < 50){
+    alert('Uneli ste pogresne mere')
+}
